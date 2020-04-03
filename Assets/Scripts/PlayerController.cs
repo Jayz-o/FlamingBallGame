e15c9e3f7bb054a549f3b82aa7c56c90f6473c6d
JARRED ORFAO <jarred.orfao97@gmail.com>
2020-04-03 16:25:34 +0200

    Player movement complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private float movementSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidBody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate() //use fixed update for physics movements
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //getting the input from the arrow keys
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        this.rigidBody.AddForce(movement * this.movementSpeed);
+    }
+}
