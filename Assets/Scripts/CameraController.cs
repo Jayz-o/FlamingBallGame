30d538cea288710cb5046af543402341c8e9993a
JARRED ORFAO <jarred.orfao97@gmail.com>
2020-04-03 16:31:07 +0200

    Camera Follow Player Complete

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;//get the difference in the position of the player and the camera
+    }
+
+    private void LateUpdate()//smoother update for camera
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
